4837ef1d610f5fb9479f5e77f5c5603c7de9df2b
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-02-13 19:38:39 +0900

    프로그래머스 LV.0 자릿수더하기

---
+++ programers/lv0/AddNum.java
@@ -0,0 +1,17 @@
+package programers.lv0;
+
+public class AddNum {
+    public static int solution(int n) {
+        char[] chars = String.valueOf(n).toCharArray();
+        int answer = 0;
+        for (char aChar : chars) {
+            answer += Integer.parseInt(String.valueOf(aChar));
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int solution = solution(1234);
+        System.out.println("solution = " + solution);
+    }
+}
