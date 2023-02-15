a40742b493c57ad10a92036dae94a9e8247babc8
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-02-15 21:56:42 +0900

    프로그래머스 LV.0 문자열 계산하기

--- programers/lv0/CalStr.java
+++ programers/lv0/CalStr.java
@@ -3,30 +3,11 @@ package programers.lv0;
 import java.util.Arrays;
 
 public class CalStr {
-    public static int solution(String my_string) {
-        String[] split = my_string.split(" ");
-        int[] ints = Arrays.stream(split).filter(s -> {
-            try {
-                Integer.parseInt(s);
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }).mapToInt(Integer::parseInt).toArray();
-        int cnt = split.length - ints.length;
+        public static int solution(String my_string) {
 
-        int answer = Integer.parseInt(split[0]);
-        int num = 1;
-        for (int i = 0; i < cnt; i++) {
-            if ("+".equals(split[num])) {
-                answer += Integer.parseInt(split[num + 1]);
-            } else {
-                answer -= Integer.parseInt(split[num + 1]);
-            }
-            num += 2;
+            String[] s = my_string.replace("- ", "-").replaceAll("[+] ", "").split(" ");
+            return Arrays.stream(s).mapToInt(Integer::parseInt).sum();
         }
-        return answer;
-    }
 
     public static void main(String[] args) {
         int solution = solution("3 + 4 - 9 + 15 + 11");
