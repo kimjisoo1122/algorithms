4667f86e5d88c55d854e662e784fb851bf392265
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-04-04 21:27:54 +0900

    프로그래머스 LV.0 배열역순

---
+++ programers/lv0/Solution.java
@@ -0,0 +1,29 @@
+package programers.lv0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public static int[] solution(int[] numbers, String direction) {
+        List<Integer> list = new ArrayList<>();
+        if ("right".equals(direction)) {
+            list.add(numbers[numbers.length - 1]);
+            for (int i = 0; i < numbers.length - 1; i++) {
+                list.add(numbers[i]);
+            }
+        } else {
+            for (int i = 1; i < numbers.length; i++) {
+                list.add(numbers[i]);
+            }
+            list.add(numbers[0]);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] solution = solution(new int[]{4, 455, 6, 4, -1, 45, 6}, "right");
+        System.out.println(Arrays.toString(solution));
+
+    }
+}
