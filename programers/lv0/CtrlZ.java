c05399ef728fa2683c63ccdb471448b1951be243
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-04-14 16:00:47 +0900

    프로그래머스 LV.0 ThrowBall

---
+++ programers/lv0/CtrlZ.java
@@ -0,0 +1,27 @@
+package programers.lv0;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class CtrlZ {
+    public int[] solution(int n) {
+
+        Set<Integer> set = new LinkedHashSet<>();
+        int divisor = 2;
+        while (n > 1) {
+            if (n % divisor == 0) {
+                set.add(divisor);
+                n /= divisor;
+            } else {
+                divisor++;
+            }
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+    public static void main(String[] args) {
+        CtrlZ ctrlZ = new CtrlZ();
+        int[] solution = ctrlZ.solution(12);
+        System.out.println("Arrays.toString(solution) = " + Arrays.toString(solution));
+    }
+}
