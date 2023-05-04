efad9d417e023d5d173665004e4eae5418b849a6
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-05-04 23:38:28 +0900

    프로그래머스 LV.1

--- programers/lv1/StarngeWord.java
+++ programers/lv1/StarngeWord.java
@@ -1,29 +1,35 @@
 package programers.lv1;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class StarngeWord {
     public String solution(String s) {
-        return Arrays.stream(s.split(" "))
-                .map(s2 -> {
-                    String[] split = s2.split("");
-                    for (int i = 0; i < split.length; i++) {
-                        System.out.println("s2 = " + s2);
-                        if (i % 2 == 0) {
-                            split[i] = split[i].toUpperCase();
-
-                        } else {
-                            split[i] = split[i].toLowerCase();
-                        }
-                    }
-                    return String.join("", split);
-                })
-                .collect(Collectors.joining(" "));
+        String[] arr = s.split(" ");
+        boolean flag = true;
+        for (int i = 0; i < arr.length; i++) {
+            String[] split = arr[i].split("");
+            for (int j = 0; j < split.length; j++) {
+                if (split[j].isEmpty()) {
+                    flag = false;
+                    continue;
+                }
+                if (flag) {
+                    split[j] = split[j].toUpperCase();
+                    flag = false;
+                } else {
+                    split[j] = split[j].toLowerCase();
+                    flag = true;
+                }
+            }
+            arr[i] = String.join("", split);
+        }
+        return String.join("", arr);
     }
 
     public static void main(String[] args) {
-        String solution = new StarngeWord().solution(" try hello world");
+        String solution = new StarngeWord().solution("try  hello world");
         System.out.println("solution = " + solution);
     }
 }
