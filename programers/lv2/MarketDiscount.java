80e6c704a7eb061c2d47f9efb03e0e340126ccb0
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-06-17 12:06:49 +0900

    프로그래머스 LV.2

--- programers/lv2/MarketDiscount.java
+++ programers/lv2/MarketDiscount.java
@@ -1,34 +1,41 @@
 package programers.lv2;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class MarketDiscount {
     public static int solution(String[] want, int[] number, String[] discount) {
 
-        for (int i = 0; i < want.length ; i++) {
-            want[i] = want[i].repeat(number[i]);
-        }
+        Map<String, Long> map = Arrays.stream(discount)
+                .limit(10)
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
 
-        int answer = 0;
-        for (int i = 0; i < discount.length; i++) {
-            String discounts = Arrays.stream(discount)
-                    .skip(i)
-                    .limit(10)
-                    .sorted()
-                    .collect(Collectors.joining());
-            int cnt = 0;
-            for (String item : want) {
-                if (discounts.contains(item)) {
-                    cnt++;
+        int day = 0;
+        int cnt = 0;
+
+        while (day <= discount.length - 10) {
+            boolean flag = true;
+            for (int i = 0; i < want.length; i++) {
+                Long discountQuantity = map.get(want[i]);
+                if (discountQuantity == null || discountQuantity < number[i]) {
+                    flag = false;
+                    break;
                 }
             }
-            if (cnt == want.length) {
-                answer = i + 1;
-                break;
+            if (flag) cnt++;
+
+            if (discount.length > 10 + day) {
+                map.computeIfPresent(discount[day], (k, v) -> Math.max(0, v - 1));
+                Long ifAbsent = map.putIfAbsent(discount[10 + day], 1L);
+                if (ifAbsent != null) {
+                    map.put(discount[10 + day], ifAbsent + 1);
+                }
             }
+            day++;
         }
-        return answer;
+
+        return cnt;
     }
 
     public static void main(String[] args) {
